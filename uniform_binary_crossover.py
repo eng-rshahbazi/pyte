e19e7c4e63fc33541b2e87f2716b83a04c9e91ba
eng-rshahbazi <151156649+eng-rshahbazi@users.noreply.github.com>
2023-12-21 00:54:41 +0330

    Create uniform_binary_crossover.py

---
+++ uniform_binary_crossover.py
@@ -0,0 +1,14 @@
+import random
+
+def uniform_binary_crossover(parent_one, parent_two):
+    string_length = len(parent_one)
+    child_one = ""
+    child_two = ""
+    for i in range(string_length):
+        if random.random() < 0.5:
+            child_one += parent_one[i]
+            child_two += parent_two[i]
+        else:
+            child_one += parent_two[i]
+            child_two += parent_one[i]
+    return child_one, child_two
